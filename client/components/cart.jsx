562a5abe49c097d87963d85d9037f878840c8d63
Joshua Vial <joshua@enspiral.com>
2019-02-19 16:25:27 +1300

    displaying total

--- client/components/cart.jsx
+++ client/components/cart.jsx
@@ -4,10 +4,14 @@ import {connect} from 'react-redux'
 class Cart extends React.Component {
     render () {
       let {cart} = this.props
+      let total = cart.reduce((acc,item) =>  {
+        return acc + item.price * item.quantity
+      }, 0)
       return <div className='cart'>
         <ul>
           {cart.map((item, i) => <li key={i}> {item.title} x {item.quantity}</li>)}
         </ul>
+        Total: ${total}
       </div>
     }
 }
